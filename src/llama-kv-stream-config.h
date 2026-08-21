832ce53813cae8b6558f0c2575dc8520bd62a624
Raymond Huang <raymond860909@gmail.com>
2026-08-20 23:36:52 -0500

    kv-stream: partition resident pages per layer

--- src/llama-kv-stream-config.h
+++ src/llama-kv-stream-config.h
@@ -22,3 +22,24 @@ struct llama_kv_stream_config_result {
 };
 
 llama_kv_stream_config_result llama_kv_stream_config_validate(const llama_kv_stream_config & config);
+
+struct llama_kv_stream_pool_layout_params {
+    uint64_t pool_bytes = 0;
+    uint64_t page_bytes = 0;
+    uint32_t layer_count = 0;
+    uint32_t scratch_pages = 0;
+};
+
+struct llama_kv_stream_pool_layout {
+    bool valid = false;
+    std::string error;
+
+    uint32_t resident_pages_per_layer  = 0;
+    uint32_t resident_tokens_per_layer = 0;
+    uint64_t scratch_bytes  = 0;
+    uint64_t resident_bytes = 0;
+    uint64_t unused_bytes   = 0;
+};
+
+llama_kv_stream_pool_layout llama_kv_stream_pool_layout_make(
+    const llama_kv_stream_pool_layout_params & params);
